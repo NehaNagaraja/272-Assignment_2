5c1b4ebd08d3a60e43c935df5838560d063796e0
NehaNagaraja <31573073+NehaNagaraja@users.noreply.github.com>
2017-11-22 23:42:05 -0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,72 @@
+'''
+################################## client.py #############################
+# 
+################################## client.py #############################
+'''
+import rocksdb
+from random import *
+import replicator_pb2
+import replicator_pb2_grpc
+import grpc
+import argparse
+from concurrent import futures
+
+PORT = 3000
+clientdb = rocksdb.DB("clientdb.db", rocksdb.Options(create_if_missing=True))
+keys =[]
+
+class ReplicatorClient():
+   def __init__(self, host='0.0.0.0', port=PORT):
+        self.channel = grpc.insecure_channel('%s:%d' % (host, port))
+        self.stub = replicator_pb2.ReplicatorStub(self.channel)
+
+   def GetInstance(self,request):
+       return self.stub.GetInstance(replicator_pb2.Request(request=request))
+
+def put():
+    value = input("Enter value to Put: ")
+    key= str(randint(1,500))
+    while key in keys:
+        key = str(randint(1,500))
+    keys.append(key)
+    print("Key is : "+key)
+    key = key.encode('ASCII')
+    value = value.encode('ASCII')
+    clientdb.put(key,value)
+    print(serverdb.get(key))
+    return key
+
+def delete():
+    key = input("Enter key to delete: ")
+    while key not in keys:
+        print("Key not in database")
+    keys.remove(key)
+    key = key.encode('ASCII')
+    serverdb.delete(key)
+    return key
+
+def update():
+    key = input("Enter key to update: ")
+    while key not in keys:
+        print("key value not in database")
+    value = input("Enter the value to be updated: ")
+    key = key.encode('ASCII')
+    value = value.encode('ASCII')
+    serverdb.put(key,value)
+    return key
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host", help="display a square of a given number")
+    args = parser.parse_args()
+    print("Client is connecting to Server at {}:{}...".format(args.host, PORT))
+    client = ReplicatorClient(host=args.host)
+    response = client.GetInstance('connect')
+    for resp in response:
+        print(resp.uid)
+    
+
+if __name__=="__main__":
+    run()
+
+
---
+++ server.py
@@ -0,0 +1,86 @@
+'''
+################################## server.py #############################
+# Lab1 gRPC RocksDB Server 
+################################## server.py #############################
+'''
+import time
+import grpc
+import replicator_pb2
+import replicator_pb2_grpc
+import uuid
+import rocksdb
+
+from concurrent import futures
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+serverdb = rocksdb.DB("server.db", rocksdb.Options(create_if_missing=True))
+keys =[]
+
+class ReplicatorServicer(replicator_pb2_grpc.ReplicatorServicer):
+    def __init__(self,f):
+        self.f =f
+
+    def __call__(self):
+        self.key = self.f()
+        self.value = masterDB.get(self.key)
+        print ("key = "+ self.key.decode())
+        print("value = "+self.value.decode())
+
+    def GetInstance(self,request,context):
+        return replicator_pb2_grpc.Response(operation=self.f,key=self.key,value=self.value)
+
+@ReplicatorServicer
+def put():
+    value = input("Enter value to Put: ")
+    key= str(randint(1,500))
+    while key in keys:
+        key = str(randint(1,500))
+    keys.append(key)
+    print("Key is : "+key)
+    key = key.encode('ASCII')
+    value = value.encode('ASCII')
+    serverdb.put(key,value)
+    print(serverdb.get(key))
+    return key
+
+@ReplicatorServicer
+def delete():
+    key = input("Enter key to delete: ")
+    while key not in keys:
+        print("Key not in database")
+    keys.remove(key)
+    key = key.encode('ASCII')
+    serverdb.delete(key)
+    return key
+
+@ReplicatorServicer
+def update():
+    key = input("Enter key to update: ")
+    while key not in keys:
+        print("key value not in database")
+    value = input("Enter the value to be updated: ")
+    key = key.encode('ASCII')
+    value = value.encode('ASCII')
+    serverdb.put(key,value)
+    return key
+
+def run(host,port):
+    print("Server started at...%d" % port)
+    while(1):
+       operation = input("Please specify the operaion to perform : \nEnter \n1. PUT\n2. DELETE \n3. UPDATE\n")
+       if(operation=="1"):
+           put()
+       elif(operation=="2"):
+           delete()
+       elif(operation=="3"):
+           update()
+
+       server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+       replicate_pb2_grpc.add_ReplicateServicer_to_server(Replicate(operation), server)
+       server.add_insecure_port('%s:%d' % (host, port))
+       server.start()
+
+if __name__ == '__main__':
+    run('0.0.0.0', 3000)
+
